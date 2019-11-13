c042ea903450e4427f4c0f658517cd87d5a90578
kennethbchen <kennethbchen@gmail.com>
2019-11-13 13:37:06 -0500

    Added Directory checking for NFC Activity

--- app/src/main/java/com/kennethbchen/smash2152scoutingsystem/NFCActivity.kt
+++ app/src/main/java/com/kennethbchen/smash2152scoutingsystem/NFCActivity.kt
@@ -2,11 +2,32 @@ package com.kennethbchen.smash2152scoutingsystem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kennethbchen.smash2152scoutingsystem.lib.ResetBehavior
+import java.io.File
 
 class NFCActivity : AppCompatActivity() {
+
+    var rootDir: File? = null
+    var stagedDir: File? = null
+    var backupDir: File? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nfc)
+
+        deepSpace.resetBehaviors[0] = ResetBehavior.persist
+        deepSpace.resetBehaviors[2] = ResetBehavior.increment
+        deepSpace.resetBehaviors[3] = ResetBehavior.persist
+
+        rootDir = this.filesDir
+        stagedDir = File(rootDir, "Staged")
+        if(stagedDir?.exists() == false){
+            stagedDir?.mkdir()
+        }
+
+        backupDir = File(rootDir, "Backup")
+        if(backupDir?.exists() == false){
+            backupDir?.mkdir()
+        }
     }
 }
